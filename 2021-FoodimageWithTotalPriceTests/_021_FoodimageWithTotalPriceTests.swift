85f95b0030815b2d84ac21ade751e312ef9560d6
Anh Tran <lamanh309@gmail.com>
2021-09-28 20:28:46 -0700

    Initial Commit

---
+++ 2021-FoodimageWithTotalPriceTests/_021_FoodimageWithTotalPriceTests.swift
@@ -0,0 +1,33 @@
+//
+//  _021_FoodimageWithTotalPriceTests.swift
+//  2021-FoodimageWithTotalPriceTests
+//
+//  Created by Anh Tran on 9/28/21.
+//
+
+import XCTest
+@testable import _021_FoodimageWithTotalPrice
+
+class _021_FoodimageWithTotalPriceTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
