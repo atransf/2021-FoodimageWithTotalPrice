5480b66a8cc40f913280f0cb1f96f98c2e04a371
Anh Tran <lamanh309@gmail.com>
2021-09-28 22:45:03 -0700

    food total price

--- 2021-FoodimageWithTotalPrice/ViewController.swift
+++ 2021-FoodimageWithTotalPrice/ViewController.swift
@@ -8,12 +8,78 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var itemNameAndPrice: UILabel!
+    @IBOutlet weak var tipInput: UITextField!
+    @IBOutlet weak var total: UILabel!
+    
+    var imageText = ""
+    var imageIndex = 0
+    var priceIndex = 0
+    let dishName = ["pizza","salad","hamburger","pasta"]
+    let priceList = [19.99,11.99,15.99,17.99]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tipInput.delegate = self
+        let imageName = dishName[imageIndex]
+        let itemPrice = priceList[priceIndex]
+        imageView.image = UIImage(named: imageName)
+        itemNameAndPrice.text = "\(imageName) price: $\(itemPrice)".capitalizingFirstLetter()
+        imageText = imageName
+        
     }
 
+    @IBAction func nextDishButtonPressed(_ sender: UIButton) {
+        if imageIndex < dishName.count-1 {
+            imageIndex += 1
+            priceIndex += 1
+        } else {
+            imageIndex = 0
+            priceIndex = 0
+        }
+        
+        let imageName = dishName[imageIndex]
+        let itemPrice = priceList[priceIndex]
+        imageView.image = UIImage(named: imageName)
+        itemNameAndPrice.text = "\(imageName) price: $\(itemPrice)".capitalizingFirstLetter()
+        imageText = imageName
+    }
+    
+    @IBAction func orderDishButtonPressed(_ sender: UIButton) {
+        let customerTip :Double? = Double(tipInput.text!)
+        var foodPrice = 0.0
+        var totalPrice = 0.0
+        let myDishToOrder = imageText
+        
+        for index in 0 ..< dishName.count {
+            if dishName[index] == myDishToOrder {
+                foodPrice = priceList[index]
+            }
+        }
+//        totalPrice = round((foodPrice + (foodPrice * (customerTip!/100)))*100) / 100.0
+//        total.text = "Total Payment: $\(totalPrice)"
+        
+        totalPrice = foodPrice + (foodPrice * (customerTip!/100))
+        total.text = "Total Payment: $" + String(format: "%.2f", totalPrice)
+    }
 
+}
+
+    
+extension String {
+    func capitalizingFirstLetter() -> String {
+        return prefix(1).capitalized + dropFirst()
+    }
+
+    mutating func capitalizeFirstLetter() {
+        self = self.capitalizingFirstLetter()
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
